2f7ffb9f3ca5a2c4991048aac05161a797738bb0
John Lindquist <johnlindquist@gmail.com>
2013-04-18 13:04:13 -0600

    working on 00_note_done

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,6 @@
 /**
- *  TypeScript is a "Preview"
- *  http://www.typescriptlang.org/
  *
- *  current   TypeScript 0.8.3
- *  date  Wed Feb 27, 2013 at 1:00 AM
- *  status  Alpha
+ *
+ *  https://github.com/borisyankov/DefinitelyTyped
  *
  */
