988545e3adf8b2bf86a8bcc88e23d9472e706407
John Lindquist <johnlindquist@gmail.com>
2013-04-18 12:14:51 -0600

    first commit

--- test/appSpec.js
+++ test/appSpec.js
@@ -1,13 +1,1 @@
-describe("Does something", function () {
-    it("should do something", function () {
-        var someClass = new foo.SomeClass("foo");
-        expect(someClass.someValue).toBe("foo");
-    });
-});
-describe("Does something", function () {
-    it("should do something", function () {
-        var someClass = new foo.SomeClass("foo");
-        expect(someClass.someValue).toBe("foo");
-    });
-});
 //@ sourceMappingURL=appSpec.js.map
