6343674ed60bc65081382c55ba015447d50306c4
John Lindquist <johnlindquist@gmail.com>
2013-04-19 18:27:32 -0700

    updating modules example

--- app/app.js
+++ app/app.js
@@ -1,15 +1,19 @@
-var Car = (function () {
-    function Car(type, color) {
-        this.type = type;
-        this.color = color;
-        this.drive = function (direction) {
-            console.log("Driving a " + this.color + " " + this.type + direction);
-            return 10;
-        };
-    }
-    return Car;
-})();
-var car = new Car("beetle", "red");
+var vehicles;
+(function (vehicles) {
+    var Car = (function () {
+        function Car(type, color) {
+            this.type = type;
+            this.color = color;
+            this.drive = function (direction) {
+                console.log("Driving a " + this.color + " " + this.type + direction);
+                return 10;
+            };
+        }
+        return Car;
+    })();
+    vehicles.Car = Car;    
+})(vehicles || (vehicles = {}));
+var car = new vehicles.Car("beetle", "red");
 var vehicles = new Array();
 vehicles.push(car);
 for(var i = 0; i < vehicles.length; i++) {
--- app/app.ts
+++ app/app.ts
@@ -1,27 +1,34 @@
 /**
- *  Interfaces
+ *  modules
  */
 
-interface Drivable{
-	type:string;
-	drive: (direction:string) => number;
+module vehicles{
+
+	export interface Drivable{
+		type:string;
+		drive: (direction:string) => number;
+	}
+
+	export class Car implements Drivable {
+		constructor(public type:string, public color:string) {
+		}
+
+		drive = function (direction:string):number {
+			console.log("Driving a " + this.color + " " + this.type + direction);
+			return 10;
+		};
+	}
 }
 
-class Car implements Drivable{
-	constructor(public type:string, public color:string){}
 
-	drive = function(direction:string):number{
-		console.log("Driving a " + this.color + " " + this.type + direction);
-		return 10;
-	};
-}
+var car:vehicles.Drivable = new vehicles.Car("beetle", "red");
 
-var car:Drivable = new Car("beetle", "red");
-
-var vehicles:Drivable[] = new Drivable[];
+var vehicles:vehicles.Drivable[] = new vehicles.Drivable[];
 vehicles.push(car);
 
 for (var i = 0; i < vehicles.length; i++) {
-	var vehicle:Drivable = vehicles[i];
+	var vehicle:vehicles.Drivable = vehicles[i];
 	vehicle.drive("north");
 }
+
+
